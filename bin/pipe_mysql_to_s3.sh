f50ea261b21fd73a01361a4efe629c5cc3bb2674
lifeofguenter <gunter@grodotzki.co.za>
2015-11-20 01:30:40 +0200

    purge older backups

--- bin/pipe_mysql_to_s3.sh
+++ bin/pipe_mysql_to_s3.sh
@@ -14,7 +14,7 @@ source "${__DIR__}/shlib/variables.shlib"
 
 
 # check if required vars were set
-if ! variables::isset MYSQL_HOST MYSQL_USER MYSQL_PASS MYSQL_DB AWS_PROFILE S3_BUCKET FILE_PREFIX; then
+if ! variables::isset MYSQL_HOST MYSQL_USER MYSQL_PASS MYSQL_DB AWS_PROFILE S3_BUCKET FILE_PREFIX NUM_BACKUPS; then
   echo 'Please make sure that the following envvars are set:' 1>&2
   echo '' 1>&2
   echo 'MYSQL_HOST' 1>&2
@@ -24,8 +24,13 @@ if ! variables::isset MYSQL_HOST MYSQL_USER MYSQL_PASS MYSQL_DB AWS_PROFILE S3_B
   echo 'AWS_PROFILE' 1>&2
   echo 'S3_BUCKET' 1>&2
   echo 'FILE_PREFIX' 1>&2
+  echo 'NUM_BACKUPS' 1>&2
   exit 1
 fi
+
+#
+# create new backup
+#
 
 filename="${FILE_PREFIX}-$(date +%Y-%m-%d).zip"
 
@@ -45,3 +50,29 @@ aws \
 s3 cp "${filename}" "s3://${S3_BUCKET}"
 
 rm "${filename}"
+
+
+#
+# purge older backups
+#
+
+# get a list of all files (this might get extremly slow on larger lists?)
+aws --profile "${AWS_PROFILE}" --output text s3 ls "s3://${S3_BUCKET}" |
+# we are only interested in our backps
+grep "${FILE_PREFIX}" |
+# squash repeating spaces
+tr -s ' ' |
+# sort by (and only by) the 1st column DESC
+sort -k1,1r |
+# loop through the results
+while read -r each_date each_time each_size each_file; do
+  n=$((n + 1))
+
+  # delete older snapshots after the desired amount is reached
+  if [ "${n}" -gt "${NUM_BACKUPS}" ]; then
+    echo "[Deleting] ${each_file} - ${each_date}"
+    aws --profile "${AWS_PROFILE}" --output text s3 rm "s3://${S3_BUCKET}/${each_file}"
+  fi
+done
+
+exit 0
