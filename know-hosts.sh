3660dc81d828b4cf5f00baf6e6ce88a962c04ac2
Günter Grodotzki <gunter@grodotzki.com>
2022-01-12 00:13:15 +0100

    some fixes (#14)

--- know-hosts.sh
+++ know-hosts.sh
@@ -17,19 +17,21 @@ add_to_inventory() {
   for host in "${@}"; do
     consolelog "adding ${host} to inventory..."
 
+    bofh_user_exists="$(remote_exec "${SSH_USER}@${host}#${SSH_PORT}" "id -u bofh 2> /dev/null || echo error")"
+
     if [[ -z "${ssh_pass}" ]]; then
-      echo "${host} ansible_user=${SSH_USER}" >> inventory-temp
-      remote_exec "${SSH_USER}@${host}" "dpkg -l | grep sudo || (apt-get -qq update && DEBIAN_FRONTEND=noninteractive apt-get -y -qq install sudo)"
-      remote_exec "${SSH_USER}@${host}" "sudo DEBIAN_FRONTEND=noninteractive apt-get -y -qq install python"
-      if [[ "${SSH_USER}" != "bofh" ]]; then
-        remote_exec "${SSH_USER}@${host}" "id -u bofh || sudo useradd -d /home/bofh -m -s /bin/bash bofh"
+      echo "${host}:${SSH_PORT} ansible_user=${SSH_USER}" >> inventory-temp
+      remote_exec "${SSH_USER}@${host}#${SSH_PORT}" "dpkg -l | grep sudo || (apt-get -qq update && DEBIAN_FRONTEND=noninteractive apt-get -y -qq install sudo)"
+      remote_exec "${SSH_USER}@${host}#${SSH_PORT}" "sudo DEBIAN_FRONTEND=noninteractive apt-get -y -qq install python"
+      if [[ "${SSH_USER}" != "bofh" ]] && [[ "${bofh_user_exists}" == "error" ]]; then
+        remote_exec "${SSH_USER}@${host}#${SSH_PORT}" "id -u bofh || sudo useradd -d /home/bofh -m -s /bin/bash bofh"
       fi
     else
-      echo "${host} ansible_user=${SSH_USER} ansible_ssh_pass=${ssh_pass} ansible_sudo_pass=${ssh_pass}" >> inventory-temp
-      remote_exec "${SSH_USER}@${host}" "dpkg -l | grep sudo || (apt-get -qq update && DEBIAN_FRONTEND=noninteractive apt-get -y -qq install sudo)" "${ssh_pass}"
-      remote_exec "${SSH_USER}@${host}" "echo '${ssh_pass}' | sudo -S DEBIAN_FRONTEND=noninteractive apt-get -y -qq install python" "${ssh_pass}"
-      if [[ "${SSH_USER}" != "bofh" ]]; then
-        remote_exec "${SSH_USER}@${host}" "echo '${ssh_pass}' | sudo -S useradd -d /home/bofh -m -s /bin/bash bofh" "${ssh_pass}"
+      echo "${host}:${SSH_PORT} ansible_user=${SSH_USER} ansible_ssh_pass=${ssh_pass} ansible_sudo_pass=${ssh_pass}" >> inventory-temp
+      remote_exec "${SSH_USER}@${host}#${SSH_PORT}" "dpkg -l | grep sudo || (apt-get -qq update && DEBIAN_FRONTEND=noninteractive apt-get -y -qq install sudo)" "${ssh_pass}"
+      remote_exec "${SSH_USER}@${host}#${SSH_PORT}" "echo '${ssh_pass}' | sudo -S DEBIAN_FRONTEND=noninteractive apt-get -y -qq install python" "${ssh_pass}"
+      if [[ "${SSH_USER}" != "bofh" ]] && [[ "${bofh_user_exists}" == "error" ]]; then
+        remote_exec "${SSH_USER}@${host}#${SSH_PORT}" "echo '${ssh_pass}' | sudo -S useradd -d /home/bofh -m -s /bin/bash bofh" "${ssh_pass}"
       fi
     fi
   done
@@ -43,7 +45,7 @@ add_to_known_hosts() {
     host_ip="$(dig +short "${host}")"
     ssh-keygen -f ~/.ssh/known_hosts -R "${host}" &> /dev/null
     ssh-keygen -f ~/.ssh/known_hosts -R "${host_ip}" &> /dev/null
-    ssh-keyscan -T 30 -p 22 "${host}" >> ~/.ssh/known_hosts 2> /dev/null
+    ssh-keyscan -T 30 -p "${SSH_PORT}" "${host}" >> ~/.ssh/known_hosts 2> /dev/null
   done
 }
 
@@ -55,6 +57,7 @@ bootstrap ansible hosts
 Options:
     -h              display this help and exit
     -u              initial ssh user (default: bofh)
+    -p              ssh-port to use (default: 22)
 
 Tasks:
     known-hosts     add HOSTS to known_hosts
@@ -64,7 +67,7 @@ EOF
 
 OPTIND=1
 
-while getopts "hu:" opt; do
+while getopts "hu:p:" opt; do
   case "${opt}" in
     h )
       usage
@@ -72,6 +75,9 @@ while getopts "hu:" opt; do
       ;;
     u )
       SSH_USER="${OPTARG}"
+      ;;
+    p )
+      SSH_PORT="${OPTARG}"
       ;;
     '?' )
       usage >&2
@@ -83,6 +89,10 @@ shift "$((OPTIND-1))"
 
 if [[ -z "${SSH_USER}" ]]; then
   SSH_USER="bofh"
+fi
+
+if [[ -z "${SSH_PORT}" ]]; then
+  SSH_PORT="22"
 fi
 
 # tasks
@@ -110,6 +120,9 @@ case "${task}" in
 
     rm -f inventory-temp
     add_to_inventory "${@}"
+
+    # install minimal required packages
+    ansible all -i inventory-temp -m ansible.builtin.apt -ba "name=acl update_cache=yes cache_valid_time=3600"
 
     # password-less sudoer
     ansible all -i inventory-temp -m lineinfile -ba "path=/etc/sudoers.d/100-no-pass-users line='bofh ALL=(ALL) NOPASSWD:ALL' create=yes mode=0440 validate='/usr/sbin/visudo -cf %s'"
